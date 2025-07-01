fd528dc6a09b760a4d40cb030e46a818a9774ff8
Shenbaga Jothi S <147464486+3012shenba@users.noreply.github.com>
2025-07-01 10:41:56 +0530

    Time: 6 ms (44.04%), Space: 44.7 MB (43.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
+        s=s.replace("\\s","").toLowerCase();
+        t=t.replace("\\s","").toLowerCase();
+        if(s.length()!=t.length())
+            return false;
         char c1[]=s.toCharArray();
         char c2[]=t.toCharArray();
         Arrays.sort(c1);
